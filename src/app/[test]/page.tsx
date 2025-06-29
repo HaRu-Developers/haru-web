26a36ec3dc4ddf76ba0a3d50f721da95f8cb04f7
duwlsssss <rladuwls0814@gmail.com>
2025-06-29 21:19:08 +0900

    :lipstick: Style: global 스타일 추가
    - p 태그에 단어 깨짐 방지, 자연스러운 줄바꿈 속성 추가

--- src/app/[test]/page.tsx
+++ src/app/[test]/page.tsx
@@ -1,7 +1,7 @@
 const TestPage = () => {
   return (
     <div className='p-8 space-y-4'>
-      <p className='text-headline-1 text-system-red'>텍스트 스타일 테스트</p>
+      <p className='text-headline-1 text-system-red'>줄바꿈이 자연스러운 한국어 문단</p>
       <button className='text-button-1 bg-gradient-primary text-white px-4 py-2 rounded'>
         버튼 스타일 테스트
       </button>

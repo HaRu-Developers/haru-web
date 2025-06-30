172a7822943540f25db77659f4f8e61f65cda27e
strfunctionk <comzun01@gmail.com>
2025-06-30 22:47:38 +0900

    🍻 Chore: @svgr/webpack 세팅

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,28 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: ['@svgr/webpack'],
+        as: '*.js',
+      },
+    },
+  },
+  webpack: (config) => {
+    const fileLoaderRule = config.module.rules.find(
+      (rule: { test?: { test?: (path: string) => boolean } }) => rule.test?.test?.('.svg'),
+    );
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: { and: [/\.(ts|tsx|js|jsx|md|mdx)$/] },
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;

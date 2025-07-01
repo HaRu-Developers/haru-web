59f4d30150fa7dc6c7ef444fe881249ca83e47ad
S-Gihun <89789115+S-Gihun@users.noreply.github.com>
2025-07-01 14:46:54 +0900

    ✨ Feat: DatePickerNum 구현

---
+++ src/common/components/DatePicker/DateNum/date-num-styles.ts
@@ -0,0 +1,13 @@
+{
+  /* 디자인 시스템 업데이트 이후 변경 예정 */
+}
+export const baseStyle =
+  'flex items-center justify-center w-9 h-9 text-body-2 rounded-[8px] transition';
+
+export const dateNumVariants = {
+  empty: 'text-gray-300 cursor-default',
+  selected: 'bg-primary text-white',
+  today: 'text-black border border-storke-200 hover:border-none hover:bg-primary',
+  past: 'text-gray-400 cursor-not-allowed',
+  available: 'text-black hover:bg-primary hover:text-white cursor-pointer',
+};

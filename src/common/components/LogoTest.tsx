d9361903408d96a714d4ba8061f4e83208f30f49
strfunctionk <84651690+strfunctionk@users.noreply.github.com>
2025-07-01 15:17:39 +0900

    [Feat] SVG 제작 (#27)
    * 🍻 Chore: @svgr/webpack 세팅
    
    * ✨ Feat: svg 파일 추가, storybook 로고 추가

---
+++ src/common/components/LogoTest.tsx
@@ -0,0 +1,113 @@
+import React from 'react';
+
+import AiManager from '@/common/svgs/component-set/AiManagerIcon.svg';
+import AiQuestionHover from '@/common/svgs/component-set/AiQuestionHoverIcon.svg';
+import AiQuestion from '@/common/svgs/component-set/AiQuestionIcon.svg';
+import ArrowLeft from '@/common/svgs/component-set/ArrowLeftIcon.svg';
+import ArrowRight from '@/common/svgs/component-set/ArrowRightIcon.svg';
+import CheckboxCircleDisable from '@/common/svgs/component-set/CheckboxCircleDisableIcon.svg';
+import CheckboxCircleEnable from '@/common/svgs/component-set/CheckboxCircleEnableIcon.svg';
+import CheckboxSquareDisable from '@/common/svgs/component-set/CheckboxSquareDisableIcon.svg';
+import CheckboxSquareEnable from '@/common/svgs/component-set/CheckboxSquareEnableIcon.svg';
+import Copy from '@/common/svgs/component-set/CopyIcon.svg';
+import CrossLine from '@/common/svgs/component-set/CrossLineIcon.svg';
+import DownloadDocs from '@/common/svgs/component-set/DownloadDocsIcon.svg';
+import DownloadPdf from '@/common/svgs/component-set/DownloadPdfIcon.svg';
+import DownloadWord from '@/common/svgs/component-set/DownloadWordIcon.svg';
+import DownloadHangul from '@/common/svgs/component-set/DownloadhangulIcon.svg';
+import Edit from '@/common/svgs/component-set/EditIcon.svg';
+import EyeClose from '@/common/svgs/component-set/EyeCloseIcon.svg';
+import EyeOpen from '@/common/svgs/component-set/EyeOpenIcon.svg';
+import GnbAiManager from '@/common/svgs/component-set/GnbAiManagerIcon.svg';
+import GnbCalender from '@/common/svgs/component-set/GnbCalenderIcon.svg';
+import GnbHome from '@/common/svgs/component-set/GnbHomeIcon.svg';
+import GnbLogout from '@/common/svgs/component-set/GnbLogoutIcon.svg';
+import GnbProfile from '@/common/svgs/component-set/GnbProfileIcon.svg';
+import GnbRecentFile from '@/common/svgs/component-set/GnbRecentFileIcon.svg';
+import GnbSetting from '@/common/svgs/component-set/GnbSettingIcon.svg';
+import GnbSnsAssist from '@/common/svgs/component-set/GnbSnsAssistIcon.svg';
+import GnbTeamMood from '@/common/svgs/component-set/GnbTeamMoodIcon.svg';
+import Link from '@/common/svgs/component-set/LinkIcon.svg';
+import Plus from '@/common/svgs/component-set/PlusIcon.svg';
+import RecordingEnd from '@/common/svgs/component-set/RecordingEndIcon.svg';
+import RecordingPause from '@/common/svgs/component-set/RecordingPauseIcon.svg';
+import RecordingPlay from '@/common/svgs/component-set/RecordingPlayIcon.svg';
+import RecordingStart from '@/common/svgs/component-set/RecordingStartIcon.svg';
+import SnsAssist from '@/common/svgs/component-set/SnsAssistIcon.svg';
+import TeamMood from '@/common/svgs/component-set/TeamMoodIcon.svg';
+import User1 from '@/common/svgs/component-set/User1Icon.svg';
+import User2 from '@/common/svgs/component-set/User2Icon.svg';
+import User3 from '@/common/svgs/component-set/User3Icon.svg';
+import User4 from '@/common/svgs/component-set/User4Icon.svg';
+import User5 from '@/common/svgs/component-set/User5Icon.svg';
+import User6 from '@/common/svgs/component-set/User6Icon.svg';
+import User7 from '@/common/svgs/component-set/User7Icon.svg';
+import ArrowDown from '@/common/svgs/individual/ArrowUnderIcon.svg';
+import Calender from '@/common/svgs/individual/CalenderIcon.svg';
+import Congratulation from '@/common/svgs/individual/CongratulationIcon.svg';
+import Download from '@/common/svgs/individual/DownloadIcon.svg';
+import Search from '@/common/svgs/individual/SearchIcon.svg';
+import Warning from '@/common/svgs/individual/WarningIcon.svg';
+import LogoMixed from '@/common/svgs/logo/LogoMixed.svg';
+import LogoSimbol from '@/common/svgs/logo/LogoSimbol.svg';
+import LogoText from '@/common/svgs/logo/LogoText.svg';
+
+const LogoTest = () => {
+  return (
+    <div className="grid grid-cols-10">
+      <AiManager height="100" width="100" />
+      <AiQuestionHover height="100" width="100" />
+      <AiQuestion height="100" width="100" />
+      <ArrowLeft height="100" width="100" />
+      <ArrowRight height="100" width="100" />
+      <CheckboxCircleDisable height="100" width="100" />
+      <CheckboxCircleEnable height="100" width="100" />
+      <CheckboxSquareDisable height="100" width="100" />
+      <CheckboxSquareEnable height="100" width="100" />
+      <Copy height="100" width="100" />
+      <CrossLine height="100" width="100" />
+      <DownloadDocs height="100" width="100" />
+      <DownloadPdf height="100" width="100" />
+      <DownloadWord height="100" width="100" />
+      <DownloadHangul height="100" width="100" />
+      <Edit height="100" width="100" />
+      <EyeClose height="100" width="100" />
+      <EyeOpen height="100" width="100" />
+      <GnbAiManager height="100" width="100" />
+      <GnbCalender height="100" width="100" />
+      <GnbHome height="100" width="100" />
+      <GnbLogout height="100" width="100" />
+      <GnbProfile height="100" width="100" />
+      <GnbRecentFile height="100" width="100" />
+      <GnbSetting height="100" width="100" />
+      <GnbSnsAssist height="100" width="100" />
+      <GnbTeamMood height="100" width="100" />
+      <Link height="100" width="100" />
+      <Plus height="100" width="100" />
+      <RecordingEnd height="100" width="100" />
+      <RecordingPause height="100" width="100" />
+      <RecordingPlay height="100" width="100" />
+      <RecordingStart height="100" width="100" />
+      <SnsAssist height="100" width="100" />
+      <TeamMood height="100" width="100" />
+      <User1 height="100" width="100" />
+      <User2 height="100" width="100" />
+      <User3 height="100" width="100" />
+      <User4 height="100" width="100" />
+      <User5 height="100" width="100" />
+      <User6 height="100" width="100" />
+      <User7 height="100" width="100" />
+      <ArrowDown height="100" width="100" />
+      <Calender height="100" width="100" />
+      <Congratulation height="100" width="100" />
+      <Download height="100" width="100" />
+      <Search height="100" width="100" />
+      <Warning height="100" width="100" />
+      <LogoMixed height="100" width="100" />
+      <LogoSimbol height="100" width="100" />
+      <LogoText height="100" width="100" />
+    </div>
+  );
+};
+
+export default LogoTest;

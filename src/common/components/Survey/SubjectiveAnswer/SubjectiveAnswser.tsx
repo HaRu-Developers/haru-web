4a2af0bce3c679085214898c5aa785fed53da33c
S-Gihun <89789115+S-Gihun@users.noreply.github.com>
2025-07-04 01:50:54 +0900

    ✨ Feat: SubjectAnswer 컴포넌트 구헌

---
+++ src/common/components/Survey/SubjectiveAnswer/SubjectiveAnswser.tsx
@@ -0,0 +1,19 @@
+import ChartContainer from '@/common/components/Survey/ChartContainer/ChartContainer';
+
+import { SubjectiveAnswersProps } from './SubjectiveAnswer.types';
+
+const SubjectiveAnswers = ({ title, answers }: SubjectiveAnswersProps) => {
+  return (
+    <ChartContainer title={title}>
+      <div className="flex w-full flex-col gap-1">
+        {answers.map((answer, index) => (
+          <div key={index} className="rounded-[5px] bg-gray-600 px-3 py-[5px]">
+            <p className="text-button-2 text-black">{answer}</p>
+            {/* T6_Sb로 대체 예정 */}
+          </div>
+        ))}
+      </div>
+    </ChartContainer>
+  );
+};
+export default SubjectiveAnswers;

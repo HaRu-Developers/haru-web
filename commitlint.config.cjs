44c120578bfc0de8e096e698938bbb2c608437cb
strfunctionk <comzun01@gmail.com>
2025-07-06 08:52:20 +0900

    🩹 Chore: gitmoji 추가 및 수정

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
     name: 'conventional-changelog-conventionalcommits',
     parserOpts: {
       headerPattern:
-        /^(✨|🔨|♻️|🐛|🚑️|💄|🎨|🔧|✏️|📝|💬|📦️|🔥|🍻|✅|⚡️|💚|🌐|🚀|🔒️|⬆️|⬇️|🚧|💡|🤔|👷|🩹)\s([A-Z][a-z]*)(?:\((.*)\))?!?: (.*)$/,
+        /^(✨|🔨|♻️|🐛|🚑️|💄|🎨|🔧|✏️|📝|💬|📦️|🔥|🩹|✅|🔀|💚|🌐|🚀|🔒️|⬆️|⬇️|🚧|💡|🤔|👷|🍻)\s([A-Z][a-z]*)(?:\((.*)\))?!?: (.*)$/,
       headerCorrespondence: ['emoji', 'type', 'scope', 'subject'],
       referenceActions: null,
       issuePrefixes: ['#'],
@@ -33,7 +33,7 @@ module.exports = {
         'Remove',
         'Chore',
         'Test',
-        'Build',
+        'Merge',
         'Ci',
         'Perf',
         'Revert',

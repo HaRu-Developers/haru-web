555203c76dfc66c5112c1f18a6e280d08139e7bb
S-Gihun <89789115+S-Gihun@users.noreply.github.com>
2025-07-07 22:32:25 +0900

    ✨ Feat: AiQuestion 컴포넌트 구현

---
+++ src/common/components/AiQuestionCard/AiQuestionCard.tsx
@@ -0,0 +1,23 @@
+import AiQuestionIcon from '@svgs/component-set/AiQuestionHoverIcon.svg';
+
+import { AiQuestionCardProps } from './AiQuestionCard.types';
+
+const AiQuestionCard = ({ aiRecommendQuestion, userAnswer }: AiQuestionCardProps) => {
+  return (
+    <div className="border-stroke-200 w-110 rounded-xl border bg-white px-5 pt-5 pb-[18px] hover:bg-gray-600">
+      <div className="mb-3 inline-flex items-start gap-3">
+        <div className="flex-shrink-0">
+          <AiQuestionIcon width="24px" height="24px" />
+        </div>
+
+        <div className="text-t6-sb text-black">{aiRecommendQuestion}</div>
+      </div>
+
+      <div className="text-cap2-rg ml-9 overflow-hidden rounded-[3px] bg-gray-600 px-2 py-[5px] text-ellipsis whitespace-nowrap text-gray-200">
+        {userAnswer}
+      </div>
+    </div>
+  );
+};
+
+export default AiQuestionCard;

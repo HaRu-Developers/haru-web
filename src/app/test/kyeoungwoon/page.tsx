4b60945f42874e7f25cfdfed5221a59e5390e88d
kyeoungwoon <saveearth1@cau.ac.kr>
2025-07-08 03:54:29 +0900

    ✨ Feat: make svgs into individual components
    기존에 svgr을 통해 번들러 단에서 처리하던 svg 파일들을 customization을 위해서 컴포넌트화 했습니다.
    
    에구 힘들다

--- src/app/test/kyeoungwoon/page.tsx
+++
@@ -1,7 +0,0 @@
-import AudioBarIcons, { AudioBarState } from '@icons/AudioBarIcons';
-
-const KyeoungwoonTestPage = () => {
-  return <AudioBarIcons state={AudioBarState.PLAY} />;
-};
-
-export default KyeoungwoonTestPage;

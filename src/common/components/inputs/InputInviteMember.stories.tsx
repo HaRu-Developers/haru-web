f44739aa1630b428411cc33c68bd5273b66fde34
strfunctionk <comzun01@gmail.com>
2025-07-09 16:44:18 +0900

    ♻️ Refactor: StoryBook 예시 추가

--- src/common/components/inputs/InputInviteMember.stories.tsx
+++ src/common/components/inputs/InputInviteMember.stories.tsx
@@ -19,3 +19,22 @@ export const Default: Story = {
     onChange: (value) => console.log(value),
   },
 };
+
+export const Search: Story = {
+  args: {
+    title: '새로운 팀원 추가하기',
+    inputValue: 'example@email.com',
+    placeholder: '초대할 팀원을 입력해주세요.',
+    onChange: (value) => console.log(value),
+  },
+};
+
+export const Add: Story = {
+  args: {
+    title: '새로운 팀원 추가하기',
+    inputEmails: ['example1@email.com', 'example2@email.com', 'example3@email.com'],
+    inputValue: 'example4@email.com',
+    placeholder: '초대할 팀원을 입력해주세요.',
+    onChange: (value) => console.log(value),
+  },
+};

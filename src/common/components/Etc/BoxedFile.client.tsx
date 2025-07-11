0d4135f226be0d94fef9833803d893cb5cfd4a38
strfunctionk <comzun01@gmail.com>
2025-07-11 17:52:14 +0900

    🩹 Chore: px -> rem 형식 변경, shadows 제거

--- src/common/components/Etc/BoxedFile.client.tsx
+++ src/common/components/Etc/BoxedFile.client.tsx
@@ -23,27 +23,27 @@ const BoxedFile = ({
     today.getMonth() === lastOpened.getMonth() &&
     today.getDate() === lastOpened.getDate();
   const time = `${isSameDate ? `마지막으로 연 시간 ${lastOpened.toLocaleTimeString()}` : lastOpened.toLocaleDateString()}`;
-  const iconClass = 'absolute top-[13px] left-[14px] h-6 w-6';
+  const iconClass = 'absolute top-13pxr left-14pxr h-6 w-6';
 
   return (
-    <div className="h-[191px] w-[244px] shrink-0 rounded-2xl bg-gray-700 px-5 py-[17px]">
+    <div className="w-244pxr h-191pxr py-17pxr shrink-0 rounded-2xl bg-gray-700 px-5">
       <div className="mb-2.5 flex w-full shrink-0 flex-col items-start gap-0.5">
         {/* 폰트 시스템에 없어서 적용 안됨 양식만 맞줘 놓음 상태 */}
         <span className="text-t5-sb">{title}</span>
         <span className="text-cap2-rg text-gray-300">{time}</span>
       </div>
       {/* shadow box 나중에 수정 예정 */}
-      <div className="shadow-boxed-file relative h-[108px] w-full shrink-0 rounded-[10px] bg-white">
+      <div className="h-108pxr rounded-10pxr relative w-full shrink-0 bg-white">
         {iconState && <FeaturedFileIcons state={iconState} className={clsx(iconClass)} />}
 
         {thumbnailUrl ? (
           <img
             src={thumbnailUrl}
             alt=""
-            className="h-full w-full rounded-[10px] object-cover object-top pt-10"
+            className="rounded-10pxr h-full w-full object-cover object-top pt-10"
           />
         ) : (
-          <div className="flex flex-col gap-1 px-3.5 pt-[47px] pb-[13px]">
+          <div className="pt-47pxr pb-13pxr flex flex-col gap-1 px-3.5">
             <div className="h-[9px] rounded-[3px] bg-gray-600" />
             <div className="h-[9px] rounded-[3px] bg-gray-600" />
             <div className="h-[9px] rounded-[3px] bg-gray-600" />

5d2bed6ba087651d65c80a1b840977c892989ad5
S-Gihun <89789115+S-Gihun@users.noreply.github.com>
2025-07-11 22:59:13 +0900

    ♻️ Refactor: px preset 사용

--- src/common/components/DatePicker/DatePickerFooter/DatePickerFooter.client.tsx
+++ src/common/components/DatePicker/DatePickerFooter/DatePickerFooter.client.tsx
@@ -7,7 +7,7 @@ import { CancelButtonType } from '@buttons/diverse-size/CancelButton/CancelButto
 import { DatePickerFooterProps } from './DatePickerFooter.types';
 
 const DatePickerFooter = ({ onConfirm, onCancel }: DatePickerFooterProps) => (
-  <div className="border-stroke-200 flex justify-end gap-[6px] rounded-b-2xl border-t bg-white pt-3 pb-4">
+  <div className="border-stroke-200 flex justify-end gap-1.5 rounded-b-2xl border-t bg-white pt-3 pb-4">
     <CancelButton buttonType={CancelButtonType.SIZE_32} onClick={onCancel} />
     <CompleteSelectButton onClick={onConfirm} />
   </div>

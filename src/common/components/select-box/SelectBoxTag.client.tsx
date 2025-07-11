6cb999c00807440cc3f5d2e6289d8b9e595e4fed
strfunctionk <comzun01@gmail.com>
2025-07-11 17:40:16 +0900

    🩹 Chore: useState 제거

--- src/common/components/select-box/SelectBoxTag.client.tsx
+++ src/common/components/select-box/SelectBoxTag.client.tsx
@@ -1,18 +1,10 @@
-import { useState } from 'react';
-
 import clsx from 'clsx';
 
 const SelectBoxTag = ({ onClick, isSelected = false, onToggle, label }: SelectBoxTagProps) => {
-  const [selected, setSelected] = useState<boolean>(isSelected);
-
   const handleClick = () => {
-    const newSelectedState = !selected;
-    setSelected(newSelectedState);
-
     if (onToggle) {
-      onToggle(newSelectedState);
+      onToggle(!isSelected);
     }
-
     if (onClick) {
       onClick();
     }
@@ -22,12 +14,12 @@ const SelectBoxTag = ({ onClick, isSelected = false, onToggle, label }: SelectBo
       className={clsx(
         'text-cap1-md h-30pxr rounded-7pxr px-9pxr flex items-center justify-center gap-2.5 py-1.5',
         {
-          'border-primary bg-primary-selected border text-black': selected === true,
-          'bg-gray-600 text-gray-200': selected === false,
+          'border-primary bg-primary-selected border text-black': isSelected === true,
+          'bg-gray-600 text-gray-200': isSelected === false,
         },
       )}
       onClick={handleClick}
-      aria-pressed={selected}
+      aria-pressed={isSelected}
     >
       {label}
     </button>

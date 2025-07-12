815b1289c0fcbcf86400ea2434ef26137ff0d2d0
duwlsssss <rladuwls0814@gmail.com>
2025-07-12 22:53:34 +0900

    ♻️ Refactor: pxr 적용

--- src/common/components/buttons/IconButton/IconButton.client.tsx
+++ src/common/components/buttons/IconButton/IconButton.client.tsx
@@ -1,12 +1,17 @@
 'use client';
 
+import clsx from 'clsx';
+
 import { IconButtonProps } from './IconButton.types';
 
 const IconButton = ({ onClick, children, className = '', ariaLabel }: IconButtonProps) => {
   return (
     <button
       onClick={onClick}
-      className={`flex h-[30px] w-[30px] items-center justify-center gap-2.5 rounded-[5px] p-1.5 hover:bg-gray-600 ${className}`}
+      className={clsx(
+        'h-30pxr w-30pxr rounded-5pxr flex items-center justify-center gap-2.5 p-1.5 hover:bg-gray-600',
+        className,
+      )}
       aria-label={ariaLabel}
     >
       {children}

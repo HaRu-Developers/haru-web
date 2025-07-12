37f04c57c6c9d378000aad1b51e8317877bcfe04
S-Gihun <89789115+S-Gihun@users.noreply.github.com>
2025-07-12 20:27:51 +0900

    ♻️ Refactor: px -> pxr

--- src/common/components/AiQuestionCard/AiQuestionCard.client.tsx
+++ src/common/components/AiQuestionCard/AiQuestionCard.client.tsx
@@ -7,7 +7,7 @@ import { AiQuestionCardProps } from './AiQuestionCard.types';
 
 const AiQuestionCard = ({ aiRecommendQuestion, userAnswer }: AiQuestionCardProps) => {
   return (
-    <div className="border-stroke-200 w-110 rounded-xl border bg-white px-5 pt-5 pb-[18px] hover:bg-gray-600">
+    <div className="border-stroke-200 w-110 rounded-xl border bg-white px-5 pt-5 pb-4.5 hover:bg-gray-600">
       <div className="mb-3 inline-flex items-start gap-3">
         <div className="flex-shrink-0">
           <AiQuestionIcons state={AiQuestionIconsState.SIZE_24_HOVER} />
@@ -16,7 +16,7 @@ const AiQuestionCard = ({ aiRecommendQuestion, userAnswer }: AiQuestionCardProps
         <div className="text-t6-sb text-black">{aiRecommendQuestion}</div>
       </div>
 
-      <div className="text-cap2-rg ml-9 overflow-hidden rounded-[3px] bg-gray-600 px-2 py-[5px] text-ellipsis whitespace-nowrap text-gray-200">
+      <div className="text-cap2-rg rounded-3pxr py-5pxr ml-9 overflow-hidden bg-gray-600 px-2 text-ellipsis whitespace-nowrap text-gray-200">
         {userAnswer}
       </div>
     </div>

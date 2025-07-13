67ddce8971b5bfddbac83f457f98b6f9413963c0
JOY <92291790+duwlsssss@users.noreply.github.com>
2025-07-13 17:11:28 +0900

    [Refactor] 셀렉트 박스_프로필 pxr, 폰트 시스템 적용 (#128)
    * ♻️ Refactor: pxr, 폰트 시스템 적용
    
    * ♻️ Refactor: clsx 적용
    
    * 💄 Ui: shadow 토큰 적용
    
    ---------
    
    Co-authored-by: kyeoungwoon <saveearth1@cau.ac.kr>

--- src/common/components/select-box/select-box-profile/WorkSpaceItem/WorkSpaceItem.server.tsx
+++ src/common/components/select-box/select-box-profile/WorkSpaceItem/WorkSpaceItem.server.tsx
@@ -9,7 +9,7 @@ const SelectBoxProfileItem = ({ workspaceId, title }: WorkSpaceItemProps) => {
   return (
     <Link
       href={linkHref}
-      className="text-b3-rg cursor-pointeritems-start flex h-8 flex-col justify-center gap-2.5 self-stretch rounded-[10px] bg-white p-2.5 hover:bg-gray-600"
+      className="text-b3-rg cursor-pointeritems-start rounded-10pxr flex h-8 flex-col justify-center gap-2.5 self-stretch bg-white p-2.5 hover:bg-gray-600"
     >
       {title}
     </Link>

02ae3f023523fedcb17f672ead37170d3c0f9efe
JOY <92291790+duwlsssss@users.noreply.github.com>
2025-07-13 14:34:39 +0900

    [Refactor] @utility -> @layer utilities 로 변경하기 (#76)
    * ✨ Feat: 푸터 컴포넌트 제작
    
    * 💄 Ui: 메인 레이아웃 스타일 변경
    
    * 💄 Ui: 메인 레이아웃 100vh 추가
    
    * ♻️ Refactor: 푸터에서에서 링크 사용 부분만 클라이언트 컴포넌트로 분리
    
    * 🐛 Bug: svg 번들링 에러 수정
    
    * 💄 Ui: 디자인 시스템 변경사항 반영
    
    * ♻️ Refactor: @layer utilities 사용
    
    * ✏️ Typo: server, client suffix 붙이기
    
    * 🐛 Bug: 로고 아이콘 경로 변경
    
    * 🔥 Remove: footer 관련 파일 삭제

--- src/app/test/joy/page.tsx
+++ src/app/test/joy/page.tsx
@@ -1,9 +1,13 @@
-import SelectBoxProfile from '@common/components/select-box/select-box-profile/SelectBoxProfile/SelectBoxProfile.server';
-
 const TestPage = () => {
   return (
-    <div className="p-10">
-      <SelectBoxProfile />
+    <div className="space-y-4 p-8">
+      <p className="text-t1-sb text-system-red">줄바꿈이 자연스러운 한국어 문단</p>
+      <button className="text-bt1-sb bg-primary-gradient rounded px-4 py-2 text-white">
+        버튼 스타일 테스트
+      </button>
+      <button className="text-bt3-sb rounded bg-gray-100 px-4 py-2 text-white">
+        버튼 스타일 테스트
+      </button>
     </div>
   );
 };

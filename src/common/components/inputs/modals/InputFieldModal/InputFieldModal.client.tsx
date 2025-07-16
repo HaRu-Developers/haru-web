d908d709513642543afde9021949c12d04d65f30
kyeoungwoon <saveearth1@cau.ac.kr>
2025-07-17 02:43:15 +0900

    🔧 Fix: 불필요하게 들어가있던 tailwindcss 클래스 제거

--- src/common/components/inputs/modals/InputFieldModal/InputFieldModal.client.tsx
+++ src/common/components/inputs/modals/InputFieldModal/InputFieldModal.client.tsx
@@ -17,7 +17,7 @@ const InputFieldModal = ({
       {title && <p className="text-cap1-rg h-16pxr text-left text-gray-200">{title}</p>}
       <input
         type={type || 'text'}
-        className="pxr py-6pxr px-12pxr text-b3-rg rounded-4pxr focus:border-stroke-selected h-36pxr flex w-full items-center justify-center border border-gray-400 text-black focus:border-2"
+        className="py-6pxr px-12pxr text-b3-rg rounded-4pxr focus:border-stroke-selected h-36pxr flex w-full items-center justify-center border border-gray-400 text-black focus:border-2"
         placeholder={placeholder}
         value={value}
         onChange={(e) => onChange(e.target.value)}

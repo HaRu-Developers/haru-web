2cd7f94994442aaaff2fc7e5ddd09f8c0a9b7d7b
S-Gihun <89789115+S-Gihun@users.noreply.github.com>
2025-07-16 18:18:54 +0900

    ♻️ Refactor: 조건문 clsx 적용

--- src/common/components/TimePicker/TimePicker.client.tsx
+++ src/common/components/TimePicker/TimePicker.client.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react';
 
 import clsx from 'clsx';
 
-import { timeSlots } from '@/utils/timePickerUtils';
+import { timeSlots } from '@common/utils/timePickerUtils';
 
 import { TimePickerProps } from './TimePicker.types';
 

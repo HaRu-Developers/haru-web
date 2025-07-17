e5ae7f20b09877a13f0226acf4065bfc6074c9d2
kyeoungwoon <saveearth1@cau.ac.kr>
2025-07-17 20:52:31 +0900

    ✨ Feat: TimePicker에서 특정 시간 이전을 비활성화할 수 있도록 했습니다

--- src/common/components/TimePicker/TimePicker.client.tsx
+++ src/common/components/TimePicker/TimePicker.client.tsx
@@ -8,13 +8,21 @@ import { timeSlots } from '@/utils/timePickerUtils';
 
 import { TimePickerProps } from './TimePicker.types';
 
-export const TimePicker = ({ onTimeSelect }: TimePickerProps) => {
+export const TimePicker = ({ onTimeSelect, timeToDisableBefore }: TimePickerProps) => {
   const [selectedTime, setSelectedTime] = useState<string | null>(null);
 
   const handleClick = (time: string) => {
     setSelectedTime(time);
     const hhmm = time.split(' ')[0];
     onTimeSelect?.(hhmm);
+  };
+
+  const isTimeDisabled = (time: string): boolean => {
+    if (!timeToDisableBefore) return false;
+    const [hh, mm] = time.split(':');
+    const timeDate = new Date(timeToDisableBefore);
+    timeDate.setHours(parseInt(hh, 10), parseInt(mm, 10));
+    return timeDate < timeToDisableBefore;
   };
 
   return (
@@ -35,6 +43,7 @@ export const TimePicker = ({ onTimeSelect }: TimePickerProps) => {
                   'border-primary bg-primary-selected border text-black': selectedTime === time,
                   'hover:bg-primary-selected border-transparent bg-gray-700 text-black':
                     selectedTime !== time,
+                  'cursor-not-allowed opacity-50': isTimeDisabled(time),
                 },
               )}
               role="option"
--- src/common/components/TimePicker/TimePicker.types.ts
+++ src/common/components/TimePicker/TimePicker.types.ts
@@ -1,3 +1,4 @@
 export interface TimePickerProps {
   onTimeSelect: (time: string) => void;
+  timeToDisableBefore?: Date;
 }

eaec51ccfae289265d82acdd022ec4821dfd9773
kyeoungwoon <saveearth1@cau.ac.kr>
2025-07-20 01:00:29 +0900

    ♻️ Refactor: || 연산자 대신 ?? 연산자 사용하도록 변경

--- src/common/components/inputs/modals/InputFieldModal/InputFieldModal.client.tsx
+++ src/common/components/inputs/modals/InputFieldModal/InputFieldModal.client.tsx
@@ -16,7 +16,7 @@ const InputFieldModal = ({
     <div className={clsx('w-534pxr gap-y-8pxr flex flex-col justify-center', className)}>
       {title && <p className="text-cap1-rg h-16pxr text-left text-gray-200">{title}</p>}
       <input
-        type={type || 'text'}
+        type={type ?? 'text'}
         className="py-6pxr px-12pxr text-b3-rg rounded-4pxr focus:border-stroke-selected h-36pxr flex w-full items-center justify-center border border-gray-400 text-black focus:border-2"
         placeholder={placeholder}
         value={value}

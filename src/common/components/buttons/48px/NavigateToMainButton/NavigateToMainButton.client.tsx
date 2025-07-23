14498f55c599afa8ef489890631751ed59a27358
kyeoungwoon <saveearth1@cau.ac.kr>
2025-07-23 19:28:03 +0900

    ♻️ Refactor: 워크스페이스 생성하기 / 메인 홈으로 이동하기 버튼 state 분리
    추후 내부 switch 를 전체 구분하는 것이 아닌 className만 구분하도록 변경할 필요 있음
    
    test page는 변경에 따른 오류가 잦아 삭제 처리, 추후 완전한 정리 및 gitignore에 추가 필요

--- src/common/components/buttons/48px/NavigateToMainButton/NavigateToMainButton.client.tsx
+++ src/common/components/buttons/48px/NavigateToMainButton/NavigateToMainButton.client.tsx
@@ -1,26 +1,39 @@
+'use client';
+
 import clsx from 'clsx';
 
-import { ButtonsCommonProps } from '../../types/buttons.common.types';
-
-interface NavigateToMainButtonProps extends ButtonsCommonProps {
-  disabled: boolean;
-}
+import { NavigateToMainButtonProps, NavigateToMainButtonState } from './NavigateToMainButton.types';
 
 /**
  * '메인 홈으로 이동' 버튼
  */
-const NavigateToMainButton = ({ disabled, className }: NavigateToMainButtonProps) => {
-  return (
-    <button
-      className={clsx(
-        'text-button-1 h-48pxr w-260pxr rounded-9pxr px-12pxr py-17pxr inline-flex items-center justify-center text-white',
-        disabled ? 'bg-gray-100' : 'bg-gray-500',
-        className,
-      )}
-    >
-      메인 홈으로 이동
-    </button>
-  );
+const NavigateToMainButton = ({ disabled, className, state }: NavigateToMainButtonProps) => {
+  switch (state) {
+    case NavigateToMainButtonState.WIDTH_260_BLACK:
+      return (
+        <button
+          className={clsx(
+            'text-bt1-sb h-48pxr w-260pxr rounded-9pxr inline-flex items-center justify-center text-white',
+            disabled ? 'bg-gray-100' : 'bg-gray-500',
+            className,
+          )}
+        >
+          메인 홈으로 이동
+        </button>
+      );
+    case NavigateToMainButtonState.WIDTH_214_WHITE:
+      return (
+        <button
+          className={clsx(
+            'text-bt1-sb h-48pxr w-214pxr rounded-9pxr border-stroke-100 inline-flex items-center justify-center border text-gray-100',
+            disabled ? '' : 'bg-white',
+            className,
+          )}
+        >
+          메인 홈으로 이동
+        </button>
+      );
+  }
 };
 
 export default NavigateToMainButton;
---
+++ src/common/components/buttons/48px/NavigateToMainButton/NavigateToMainButton.types.ts
@@ -0,0 +1,13 @@
+'use client';
+
+import { ButtonsCommonProps } from '../../types/buttons.common.types';
+
+export interface NavigateToMainButtonProps extends ButtonsCommonProps {
+  disabled: boolean;
+  className?: string;
+  state: NavigateToMainButtonState;
+}
+export enum NavigateToMainButtonState {
+  WIDTH_260_BLACK = 'w-260pxr',
+  WIDTH_214_WHITE = 'w-214pxr',
+}

ac0643388090c9f0ee79ad510081f5789f3f3e8b
duwlsssss <rladuwls0814@gmail.com>
2025-07-27 01:15:57 +0900

    ✏️ Typo: not-found 글 수정

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -20,9 +20,10 @@ const NotFound = () => {
           <h2 className="mb-14pxr text-primary h-52pr text-40pxr self-stretch leading-[130%] font-bold tracking-[-0.8px] not-italic">
             404 ERROR
           </h2>
-          <p className="mb-42pxr text-b1-rg whitespace-pre-line text-gray-200">
-            죄송합니다. 페이지를 찾을 수 없습니다. 존재하지 않는 주소를 입력하셨거나, 요청하신
-            페이지의 주소가 변경, 삭제되어 찾을 수 없습니다.
+          <p className="mb-42pxr text-b1-rg text-gray-200">
+            죄송합니다. 페이지를 찾을 수 없습니다. <br />
+            존재하지 않는 주소를 입력하셨거나, <br />
+            요청하신 페이지의 주소가 변경, 삭제되어 찾을 수 없습니다.
           </p>
           <CustomButton onClick={() => router.back()}>이전으로 이동</CustomButton>
         </div>

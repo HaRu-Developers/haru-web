94cc5f7403307b2fe4f68ab01003b20d2a247d15
duwlsssss <rladuwls0814@gmail.com>
2025-07-29 14:23:58 +0900

    ✨ Feat: 새 워크 스페이스 생성 클릭시 /onboarding으로

--- src/common/components/select-box/SelectBoxProfile/NewWorkspaceButton/NewWorkspaceButton.client.tsx
+++ src/common/components/select-box/SelectBoxProfile/NewWorkspaceButton/NewWorkspaceButton.client.tsx
@@ -1,20 +1,21 @@
 'use client';
 
+import Link from 'next/link';
+
 import PlusIcons from '@icons/PlusIcons/PlusIcons';
 import { PlusIconsState } from '@icons/PlusIcons/PlusIcons.types';
 
+import { ROUTES } from '@common/constants/routes.constants';
+
 const NewWorkspaceButton = () => {
-  const handleNewWorkspaceClick = () => {
-    console.log('새 워크스페이스 생성 버튼 클릭');
-  };
   return (
-    <button
+    <Link
       className="gap-6pxr rounded-10pxr p-10pxr flex w-full items-center justify-start self-stretch"
-      onClick={handleNewWorkspaceClick}
+      href={ROUTES.ONBOARDING}
     >
       <PlusIcons state={PlusIconsState.SIZE_20_PRIMARY} />
       <span className="text-primary text-cap1-md">내 워크스페이스 추가</span>
-    </button>
+    </Link>
   );
 };
 

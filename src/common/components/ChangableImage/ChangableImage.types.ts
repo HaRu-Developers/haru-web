f16d578a4adbc37117929012fa2010544ec7ad97
duwlsssss <rladuwls0814@gmail.com>
2025-07-29 23:25:29 +0900

    💄 Ui: 이미지 변경 컴포넌트 모달, 온보딩에 적용

---
+++ src/common/components/ChangableImage/ChangableImage.types.ts
@@ -0,0 +1,6 @@
+export interface ChangableImageProps {
+  initialPreview?: string | null; // 초기 이미지 주소
+  title: string; // 첫 글자 추출에 쓸 제목
+  onFileChange?: (file: File) => void;
+  className?: string; // 추가 스타일링 필요시 사용
+}

ddd4875840bd20c29c2f5e1560c1d6623dc45290
S-Gihun <89789115+S-Gihun@users.noreply.github.com>
2025-08-01 17:35:06 +0900

    ✨ Feat: CreatedInfo 컴포넌트 구현

---
+++ src/common/components/FileCreatedInfo/FileCreatedInfo.client.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import IndividualIcons from '@icons/IndividualIcons/IndividualIcons';
+import { IndividualIconsState } from '@icons/IndividualIcons/IndividualIcons.types';
+
+import DefaultProfileImage from '@common/components/images/DefaultProfileImage/DefaultProfileImage.client';
+import { ImageSize } from '@common/components/images/types/images.common.types';
+
+import { FileCreatedInfoProps } from './FileCreatedInfo.types';
+
+const CreatedInfo = ({ name, userId, dateTime }: FileCreatedInfoProps) => {
+  return (
+    <div className="text-cap2-md gap-5pxr py-2pxr flex font-semibold text-gray-400">
+      <DefaultProfileImage name={name} userId={userId} size={ImageSize.XSMALL} />
+      <span className="mr-11pxr">{name}</span>
+      <IndividualIcons state={IndividualIconsState.CALENDAR_SIZE_16} />
+      <span>{dateTime}</span>
+    </div>
+  );
+};
+
+export default CreatedInfo;

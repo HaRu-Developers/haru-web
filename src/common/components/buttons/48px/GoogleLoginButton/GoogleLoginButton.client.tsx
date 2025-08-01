d45f1f436c5fa74e49b3863c642110c390df3ec4
kyeoungwoon <saveearth1@cau.ac.kr>
2025-08-01 18:00:41 +0900

    ♻️ Refactor: 버튼 내에 className clsx 이용해서 중첩할 수 있도록 변경

--- src/common/components/buttons/48px/GoogleLoginButton/GoogleLoginButton.client.tsx
+++ src/common/components/buttons/48px/GoogleLoginButton/GoogleLoginButton.client.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import clsx from 'clsx';
+
 import ThirdPartyLogoIcons from '@icons/logos/ThirdPartyLogoIcon/ThirdPartyLogoIcons';
 import { ThirdPartyLogoIconsState } from '@icons/logos/ThirdPartyLogoIcon/ThirdPartyLogoIcons.types';
 
@@ -8,10 +10,13 @@ import { ButtonsCommonProps } from '../../types/buttons.common.types';
 /**
  * 'Google로 로그인하기' 버튼
  */
-const GoogleLoginButton = ({ onClick, ...props }: ButtonsCommonProps) => {
+const GoogleLoginButton = ({ className, onClick, ...props }: ButtonsCommonProps) => {
   return (
     <button
-      className="text-b3-rg border-stroke-100 h-48pxr w-414pxr gap-x-4pxr rounded-9pxr inline-flex items-center justify-center border bg-white text-gray-100"
+      className={clsx(
+        'text-b3-rg border-stroke-100 h-48pxr w-414pxr gap-x-4pxr rounded-9pxr inline-flex items-center justify-center border bg-white text-gray-100',
+        className,
+      )}
       onClick={onClick}
       {...props}
     >

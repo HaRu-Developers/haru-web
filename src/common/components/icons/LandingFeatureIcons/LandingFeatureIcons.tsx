75c49043fa415dc36a4bdb5e20136f5517209b50
strfunctionk <comzun01@gmail.com>
2025-08-02 19:30:39 +0900

    🩹 Chore: icons 색상 변경 추가

--- src/common/components/icons/LandingFeatureIcons/LandingFeatureIcons.tsx
+++ src/common/components/icons/LandingFeatureIcons/LandingFeatureIcons.tsx
@@ -5,20 +5,28 @@ import { IconsCommonProps } from '@icons/types/icons.common.types';
 import CalendarIcon from '@svgs/Landing/LandingCalendar.svg';
 import EventIcon from '@svgs/Landing/LandingEvent.svg';
 import MeetingIcon from '@svgs/Landing/LandingMeeting.svg';
-import MoodTracker from '@svgs/LandingLandingMoodTracker.svg';
+import MoodTracker from '@svgs/Landing/LandingMoodTracker.svg';
 
 import { LandingFeatureIconsState } from './LandingFeatureIcons.types';
 
 const LandingFeatureIcons = ({ state, className }: IconsCommonProps<LandingFeatureIconsState>) => {
   switch (state) {
     case LandingFeatureIconsState.SIZE_24_CALENDAR:
-      return <CalendarIcon className={clsx('h-24pxr w-24pxr', className)} />;
+      return <CalendarIcon className={clsx('h-24pxr w-24pxr text-[#EA4E43]', className)} />;
     case LandingFeatureIconsState.SIZE_24_EVENT:
-      return <EventIcon className={clsx('h-24pxr w-24pxr', className)} />;
+      return <EventIcon className={clsx('h-24pxr w-24pxr text-[#F8792A]', className)} />;
     case LandingFeatureIconsState.SIZE_24_MEETING:
-      return <MeetingIcon className={clsx('h-24pxr w-24pxr', className)} />;
+      return <MeetingIcon className={clsx('h-24pxr w-24pxr text-[#FAA700]', className)} />;
     case LandingFeatureIconsState.SIZE_24_MOODTRACKER:
-      return <MoodTracker className={clsx('h-24pxr w-24pxr', className)} />;
+      return <MoodTracker className={clsx('h-24pxr w-24pxr text-[#0A85D1]', className)} />;
+    case LandingFeatureIconsState.SIZE_24_CALENDAR_WHITE:
+      return <CalendarIcon className={clsx('h-24pxr w-24pxr text-white', className)} />;
+    case LandingFeatureIconsState.SIZE_24_EVENT_WHITE:
+      return <EventIcon className={clsx('h-24pxr w-24pxr text-white', className)} />;
+    case LandingFeatureIconsState.SIZE_24_MEETING_WHITE:
+      return <MeetingIcon className={clsx('h-24pxr w-24pxr text-white', className)} />;
+    case LandingFeatureIconsState.SIZE_24_MOODTRACKER_WHITE:
+      return <MoodTracker className={clsx('h-24pxr w-24pxr text-white', className)} />;
     case LandingFeatureIconsState.SIZE_62_EVENT:
       return <EventIcon className={clsx('h-62pxr w-62pxr', className)} />;
     case LandingFeatureIconsState.SIZE_62_MEETING:

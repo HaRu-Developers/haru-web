8aef3109a269574f243bef346feb338de201e980
strfunctionk <comzun01@gmail.com>
2025-08-02 22:54:55 +0900

    ✨ Feat: landing start 제작

---
+++ src/features/landing/components/LandingStart/LandingStart.client.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import CtaBannerButton from '@features/landing/components/buttons/cta-buttons/CtaBannerButton/CtaBannerButton.client';
+
+import { LandingStartProps } from './LandingStart.types';
+
+const LandingStart = ({ onButtonClick }: LandingStartProps) => {
+  return (
+    <div className="w-1440pxr py-92pxr gap-42pxr bg-landing-bg flex flex-col items-center justify-center">
+      {/* 배너 타이틀 영역 */}
+      <div className="text-h2-bd flex flex-col items-center">
+        <div>
+          <span className="text-black">지금 바로 </span>
+          <span className="text-primary">HaRu</span>
+          <span className="text-black">와</span>
+        </div>
+        <span className="text-black">하루를 시작해 보세요!</span>
+      </div>
+      {/* 배너 버튼 */}
+      <CtaBannerButton onClick={onButtonClick} />
+    </div>
+  );
+};
+
+export default LandingStart;
---
+++ src/features/landing/components/LandingStart/LandingStart.types.ts
@@ -0,0 +1,3 @@
+export interface LandingStartProps {
+  onButtonClick?: () => void;
+}

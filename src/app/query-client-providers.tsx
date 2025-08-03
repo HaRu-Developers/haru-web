1c1d36f301284d86246c94e7685c8aaa33fb58d5
duwlsssss <rladuwls0814@gmail.com>
2025-08-03 16:02:20 +0900

    🩹 Chore: 개발 모드에서 ReactQueryDevtools 세팅

--- src/app/query-client-providers.tsx
+++ src/app/query-client-providers.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 
-import { HydrationBoundary, QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 interface QueryClientProvidersProps {
@@ -24,7 +24,12 @@ const QueryClientProviders = ({ children, dehydratedState }: QueryClientProvider
       }),
   );
 
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      {process.env.NODE_ENV === 'development' && <ReactQueryDevtools initialIsOpen={false} />}
+    </QueryClientProvider>
+  );
 };
 
 export default QueryClientProviders;

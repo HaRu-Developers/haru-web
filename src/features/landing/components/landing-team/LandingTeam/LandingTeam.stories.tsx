013003752c66d5a4c05a973f67f837012ac828da
strfunctionk <comzun01@gmail.com>
2025-08-05 02:18:22 +0900

    🔨 Fix: 에러 수정

--- src/features/landing/components/landing-team/LandingTeam/LandingTeam.stories.tsx
+++ src/features/landing/components/landing-team/LandingTeam/LandingTeam.stories.tsx
@@ -12,9 +12,5 @@ export default meta;
 type Story = StoryObj<typeof LandingTeam>;
 
 export const Default: Story = {
-  args: {
-    name: '황지원',
-    position: 'PM',
-    description: '중앙대학교 경영 주전공, 소프트웨어벤처 융합전공',
-  },
+  args: {},
 };

47cc46a55e5cb2938df831edad5920b2ce453f2f
kyeoungwoon <saveearth1@cau.ac.kr>
2025-08-05 18:35:23 +0900

    ✨ Feat: login page 초안

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,16 @@
+import LoginOnBoarding from '@common/components/onboarding/LoginOnBoaring/LoginOnBoarding.client';
+
+import RightLoginPage from '@features/auth/components/login-page/RightLoginPage/RightLoginPage.client';
+
+const LoginPage = () => {
+  return (
+    <div className="flex h-screen w-full flex-row">
+      <LoginOnBoarding />
+      <div className="flex w-[50vw] items-center justify-center">
+        <RightLoginPage />
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;

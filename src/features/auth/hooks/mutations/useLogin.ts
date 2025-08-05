8f70bd09317f930675a9c45fada8aab3fc2d96de
Kyeoungwoon Park <saveearth1@cau.ac.kr>
2025-08-05 18:44:14 +0900

    🩹 Chore: WIP - mutation hook sample

---
+++ src/features/auth/hooks/mutations/useLogin.ts
@@ -0,0 +1,14 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+import { LoginRequestDto } from '@features/auth/types/api.types';
+
+import { login } from '@features/auth/apis/post/login-register-refresh';
+
+export const useLogin = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (data: LoginRequestDto) => login(data),
+    onSuccess: (data) => {},
+  });
+};

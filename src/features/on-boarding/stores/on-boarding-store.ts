122d9da17d71279a5f8ad52c2f0cfd9ce5972b8f
S-Gihun <89789115+S-Gihun@users.noreply.github.com>
2025-08-07 23:30:23 +0900

    ✨ Feat: Instagram 연동 API 연결

--- src/features/on-boarding/stores/on-boarding-store.ts
+++ src/features/on-boarding/stores/on-boarding-store.ts
@@ -1,5 +1,5 @@
 import { create } from 'zustand';
-import { devtools } from 'zustand/middleware';
+import { createJSONStorage, devtools, persist } from 'zustand/middleware';
 import { immer } from 'zustand/middleware/immer';
 
 import { OnboardingStep } from '../constants/step';
@@ -8,13 +8,13 @@ interface OnboardingStoreState {
   step: OnboardingStep;
   name: string;
   image: File | null;
-  workspaceId: number | null;
+  workspaceId: string | null;
   emails: string[];
   isInstagramConnected: boolean;
   actions: {
     setName: (name: string) => void;
     setImage: (file: File) => void;
-    setWorkspaceId: (id: number) => void;
+    setWorkspaceId: (workspaceId: string) => void;
     addEmail: (email: string) => void;
     removeEmail: (index: number) => void;
     setEmails: (emails: string[]) => void;
@@ -26,68 +26,81 @@ interface OnboardingStoreState {
 }
 
 const useOnboardingStore = create<OnboardingStoreState>()(
-  devtools(
-    immer((set) => ({
-      step: OnboardingStep.NAME,
-      name: '',
-      image: null,
-      workspaceId: null,
-      emails: [],
-      isInstagramConnected: false,
+  persist(
+    devtools(
+      immer((set) => ({
+        step: OnboardingStep.NAME,
+        name: '',
+        image: null,
+        workspaceId: null,
+        emails: [],
+        isInstagramConnected: false,
 
-      actions: {
-        setName: (name) =>
-          set((state) => {
-            state.name = name;
-          }),
-        setImage: (file) =>
-          set((state) => {
-            state.image = file;
-          }),
-        setWorkspaceId: (id) =>
-          set((state) => {
-            state.workspaceId = id;
-          }),
-        addEmail: (email) =>
-          set((state) => {
-            if (!state.emails.includes(email)) {
-              state.emails.push(email);
-            }
-          }),
-        removeEmail: (index) =>
-          set((state) => {
-            state.emails.splice(index, 1);
-          }),
-        setEmails: (emails) =>
-          set((state) => {
-            state.emails = emails;
-          }),
-        setInstagramConnected: (connected) =>
-          set((state) => {
-            state.isInstagramConnected = connected;
-          }),
-        nextStep: () =>
-          set((state) => {
-            if (state.step < OnboardingStep.INTEGRATE) {
-              state.step += 1;
-            }
-          }),
-        prevStep: () =>
-          set((state) => {
-            if (state.step > OnboardingStep.NAME) {
-              state.step -= 1;
-            }
-          }),
-        reset: () =>
-          set((state) => {
-            state.step = OnboardingStep.NAME;
-            state.name = '';
-            state.image = null;
-            state.emails = [];
-            state.isInstagramConnected = false;
-          }),
-      },
-    })),
+        actions: {
+          setName: (name) =>
+            set((state) => {
+              state.name = name;
+            }),
+          setImage: (file) =>
+            set((state) => {
+              state.image = file;
+            }),
+          setWorkspaceId: (id) =>
+            set((state) => {
+              state.workspaceId = id;
+            }),
+          addEmail: (email) =>
+            set((state) => {
+              if (!state.emails.includes(email)) {
+                state.emails.push(email);
+              }
+            }),
+          removeEmail: (index) =>
+            set((state) => {
+              state.emails.splice(index, 1);
+            }),
+          setEmails: (emails) =>
+            set((state) => {
+              state.emails = emails;
+            }),
+          setInstagramConnected: (connected) =>
+            set((state) => {
+              state.isInstagramConnected = connected;
+            }),
+          nextStep: () =>
+            set((state) => {
+              if (state.step < OnboardingStep.INTEGRATE) {
+                state.step += 1;
+              }
+            }),
+          prevStep: () =>
+            set((state) => {
+              if (state.step > OnboardingStep.NAME) {
+                state.step -= 1;
+              }
+            }),
+          reset: () =>
+            set((state) => {
+              state.step = OnboardingStep.NAME;
+              state.name = '';
+              state.image = null;
+              state.emails = [];
+              state.isInstagramConnected = false;
+            }),
+        },
+      })),
+    ),
+    {
+      name: 'onboarding-storage', // sessionStorage에 저장될 때 사용될 key 이름
+      storage: createJSONStorage(() => sessionStorage), // localStorage 대신 sessionStorage 사용
+      partialize: (state) => ({
+        step: state.step,
+        name: state.name,
+        workspaceId: state.workspaceId,
+        emails: state.emails,
+        isInstagramConnected: state.isInstagramConnected,
+      }),
+    },
   ),
 );
 

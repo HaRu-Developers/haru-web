6ab6154813ff1e2901c1a8079a4575b419daa61f
Kyeoungwoon Park <saveearth1@cau.ac.kr>
2025-08-12 13:25:05 +0900

    ✨ Feat: 전역 상태로 User 관리하도록 수정

--- src/features/auth/stores/auth-store.ts
+++ src/features/auth/stores/auth-store.ts
@@ -2,11 +2,12 @@ import { create } from 'zustand';
 import { devtools, persist } from 'zustand/middleware';
 import { immer } from 'zustand/middleware/immer';
 
+// TODO: BE 구현 후에 Optional 전부 제거하기
 interface User {
-  id: string;
-  name: string;
-  accessToken: string;
-  refreshToken: string;
+  id?: string;
+  name?: string;
+  accessToken?: string;
+  refreshToken?: string;
   workspaceIdList?: string[];
 }
 
@@ -34,18 +35,25 @@ const useAuthStore = create<AuthStoreState>()(
             set((state) => {
               if (state.user) {
                 state.user.accessToken = accessToken;
+              } else {
+                state.user = { accessToken };
               }
             }),
           setRefreshToken: (refreshToken) =>
             set((state) => {
               if (state.user) {
                 state.user.refreshToken = refreshToken;
+              } else {
+                state.user = { refreshToken };
               }
             }),
           setWorkspaceIdList: (workspaceIdList) =>
             set((state) => {
               if (state.user) {
                 state.user.workspaceIdList = workspaceIdList;
+              } else {
+                // TODO: 오용을 방지하기 위해 추가된 부분, 추후 수정 필요
+                throw new Error('ERROR: USER NOT SET IN AUTH STORE');
               }
             }),
         },

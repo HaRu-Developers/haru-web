3e654e5eb7510883000d58093ada813e99cfb897
strfunctionk <comzun01@gmail.com>
2025-08-14 21:38:12 +0900

    🩹 Chore: route 및 ui 수정

--- src/common/components/cta/TextCtaWrapper/TextCtaWrapper.client.tsx
+++ src/common/components/cta/TextCtaWrapper/TextCtaWrapper.client.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { useParams, useRouter } from 'next/navigation';
+
 import { FileType } from '@common/types/file-type.enum';
 
 import TextCta from '@common/components/cta/TextCta/TextCta.client';
@@ -7,14 +9,20 @@ import TextCta from '@common/components/cta/TextCta/TextCta.client';
 import { TextCtaWrapperProps } from './TextCtaWrapper.types';
 
 const TextCtaWrapper = ({ fileType }: TextCtaWrapperProps) => {
+  const router = useRouter();
+  const { workspaceId } = useParams();
   const handleClick = (fileType: FileType) => {
+    if (!workspaceId) {
+      console.warn('workspaceId를 찾을 수 없습니다.');
+      return;
+    }
     switch (fileType) {
       case FileType.AI_MEETING_MANAGER:
         console.log('새 회의 모달 열기');
         break;
 
       case FileType.SNS_EVENT_ASSISTANT:
-        console.log('새 이벤트 생성 모달 열기');
+        router.push(`/workspace/${workspaceId}/sns-event-assistant/new-event`);
         break;
 
       case FileType.TEAM_MOOD_TRACKER:

6f86cc8aa8dd09af892c4ff4933ea18fd28f7a3c
Kyeoungwoon Park <saveearth1@cau.ac.kr>
2025-08-15 00:00:02 +0900

    ♻️ Refactor: type&interface 명 직관적으로 변경
    한 번 더 리팩토링 필요

--- src/features/team-mood-tracker/components/mood-reports/answer-section/TeamMoodAnswerChartSection/TeamMoodAnswerChartSection.client.tsx
+++ src/features/team-mood-tracker/components/mood-reports/answer-section/TeamMoodAnswerChartSection/TeamMoodAnswerChartSection.client.tsx
@@ -4,7 +4,7 @@ import BarChart from '@common/components/survey/BarChart/BarChar.client';
 import PieChart from '@common/components/survey/PieChart/PieChart.client';
 import SubjectiveAnswers from '@common/components/survey/SubjectiveAnswer/SubjectiveAnswser.server';
 
-import { SurveyQuestionType } from '@features/team-mood-tracker/constants/question.constants';
+import { TeamMoodTrackerSurveyQuestionType } from '@features/team-mood-tracker/constants/question.constants';
 
 import { TeamMoodAnswerChartSectionProps } from './TeamMoodAnswerChartSection.types';
 
@@ -16,18 +16,18 @@ const TeamMoodAnswerChartSection = ({ responses }: TeamMoodAnswerChartSectionPro
       {responses.map((response) => {
         // 4. case의 문자열을 enum으로 변경하여 안정성을 높입니다.
         switch (response.type) {
-          case SurveyQuestionType.MULTIPLE_CHOICE: {
+          case TeamMoodTrackerSurveyQuestionType.MULTIPLE_CHOICE: {
             const chartData = {
-              labels: response.mulipleChoiceResponseList.map((item) => item.content),
-              values: response.mulipleChoiceResponseList.map((item) => item.selectedNum),
-              colors: PIE_CHART_COLORS.slice(0, response.mulipleChoiceResponseList.length),
+              labels: response.multipleChoiceResponseList.map((item) => item.content),
+              values: response.multipleChoiceResponseList.map((item) => item.selectedNum),
+              colors: PIE_CHART_COLORS.slice(0, response.multipleChoiceResponseList.length),
             };
             return (
               <PieChart key={response.questionId} title={response.questionTitle} data={chartData} />
             );
           }
 
-          case SurveyQuestionType.CHECKBOX_CHOICE: {
+          case TeamMoodTrackerSurveyQuestionType.CHECKBOX_CHOICE: {
             const chartData = {
               labels: response.checkboxChoiceResponseList.map((item) => item.content),
               values: response.checkboxChoiceResponseList.map((item) => item.selectedNum),
@@ -38,7 +38,7 @@ const TeamMoodAnswerChartSection = ({ responses }: TeamMoodAnswerChartSectionPro
             );
           }
 
-          case SurveyQuestionType.SUBJECTIVE:
+          case TeamMoodTrackerSurveyQuestionType.SUBJECTIVE:
             return (
               <SubjectiveAnswers
                 key={response.questionId}

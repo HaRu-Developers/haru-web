82ff609e8b1ca222f1c2f9845db2a64d6bfcc963
strfunctionk <comzun01@gmail.com>
2025-08-14 21:37:15 +0900

    ✨ Feat: sns 이벤트 어시스턴트 store 추가

--- src/common/components/modals/CreateNewEventModal/CreateNewEventModal.client.tsx
+++ src/common/components/modals/CreateNewEventModal/CreateNewEventModal.client.tsx
@@ -5,6 +5,11 @@ import { useEffect, useState } from 'react';
 import CrossIcons from '@icons/CrossIcons/CrossIcons';
 import { CrossIconsState } from '@icons/CrossIcons/CrossIcons.types';
 
+import {
+  useSnsEventAssistantActions,
+  useSnsEventAssistantInfo,
+} from '@common/hooks/stores/useSnsEventAssistantStore';
+
 import ToggleButton from '@common/components/buttons/22px/ToggleButton/ToggleButton.client';
 import NextStepButton from '@common/components/buttons/30px/NextStepButton/NextStepButton.client';
 import InputChips from '@common/components/inputs/InputChips/InputChips.client';
@@ -21,31 +26,28 @@ import { CreateNewEventModalProps } from './CreateNewEventModal.types';
  * 새로운 이벤트를 생성할 때 사용하는 모달입니다.
  */
 const CreateNewEventModal = ({ onClose, onNextStep }: CreateNewEventModalProps) => {
-  const [eventTitle, setEventTitle] = useState<string>('');
-  const [snsEventLink, setSnsEventLink] = useState<string>('');
-  const [temporaryDate, setTemporaryDate] = useState<Date | null>(null);
-  const [keyword, setKeyword] = useState<string>('');
+  const { newTitle, newSnsEventLink, friendTag, isFollowed, isLiked, keyword, period } =
+    useSnsEventAssistantInfo();
+  const {
+    setNewTitle,
+    setNewSnsEventLink,
+    changeKeyword,
+    setFriendTagRequirement,
+    setPeriod,
+    toggleFollow,
+    toggleFriendTag,
+    toggleKeyword,
+    toggleLike,
+    togglePeriod,
+  } = useSnsEventAssistantActions();
+
+  const [temporaryDate, setTemporaryDate] = useState<Date | null>(period?.endDate ?? null);
 
   useEffect(() => {
     if (temporaryDate) {
       setPeriod(temporaryDate);
     }
   }, [temporaryDate]);
-
-  const {
-    conditions,
-    toggleLike,
-    toggleFollow,
-    togglePeriod,
-    toggleKeyword,
-    toggleFriendTag,
-    setPeriod,
-    addKeyword,
-    changeKeyword,
-    removeKeyword,
-    setFriendTagRequirement,
-    reset,
-  } = useCreateEventConditions();
 
   /**
    * 친구 태그 수 변경 핸들러
@@ -61,7 +63,7 @@ const CreateNewEventModal = ({ onClose, onNextStep }: CreateNewEventModalProps) 
   };
 
   return (
-    <div className="p-24pxr rounded-16pxr w-582pxr shadow-modal flex flex-col items-center justify-center">
+    <div className="p-24pxr rounded-16pxr w-582pxr shadow-modal flex flex-col items-center justify-center bg-white">
       {/* 모달 제목 + 닫기 버튼 */}
       <div className="w-534pxr h-32pxr flex items-center justify-between">
         <CommonText type={CommonTextType.T3_BD_BLACK} text="새로운 이벤트" />
@@ -75,14 +77,14 @@ const CreateNewEventModal = ({ onClose, onNextStep }: CreateNewEventModalProps) 
         <InputFieldModal
           title="이벤트명"
           placeholder="이벤트의 제목을 입력해 주세요."
-          value={eventTitle}
-          onChange={setEventTitle}
+          value={newTitle}
+          onChange={setNewTitle}
         />
         <InputFieldModal
           title="SNS 이벤트 링크"
           placeholder="이벤트의 링크를 입력해 주세요."
-          value={snsEventLink}
-          onChange={setSnsEventLink}
+          value={newSnsEventLink}
+          onChange={setNewSnsEventLink}
         />
       </div>
 
@@ -98,16 +100,8 @@ const CreateNewEventModal = ({ onClose, onNextStep }: CreateNewEventModalProps) 
             className="mt-3pxr"
           />
           <div className="gap-x-8pxr mt-8pxr flex flex-row">
-            <SelectBoxTag
-              label="좋아요 여부"
-              onClick={toggleLike}
-              isSelected={conditions.isLiked}
-            />
-            <SelectBoxTag
-              label="팔로우 여부"
-              onClick={toggleFollow}
-              isSelected={conditions.isFollowed}
-            />
+            <SelectBoxTag label="좋아요 여부" onClick={toggleLike} isSelected={isLiked} />
+            <SelectBoxTag label="팔로우 여부" onClick={toggleFollow} isSelected={isFollowed} />
           </div>
         </div>
 
@@ -115,12 +109,12 @@ const CreateNewEventModal = ({ onClose, onNextStep }: CreateNewEventModalProps) 
         <div className="mt-12pxr relative flex w-full flex-col items-start justify-center">
           <ToggleButton
             className="absolute top-0 right-0"
-            state={conditions.period.isActive}
+            state={period.isActive}
             onToggle={() => {
               setTemporaryDate(null);
               togglePeriod();
             }}
-            initialState={conditions.period.isActive}
+            initialState={period.isActive}
           />
           <CommonText type={CommonTextType.T6_SB_BLACK} text="참여 기간" />
           <CommonText
@@ -128,7 +122,7 @@ const CreateNewEventModal = ({ onClose, onNextStep }: CreateNewEventModalProps) 
             text="아래 항목 중 선택한 조건을 만족한 참여자만 수집해 드려요."
             className="mt-3pxr"
           />
-          {conditions.period.isActive && (
+          {period.isActive && (
             <DateTimePicker
               selectedDateTime={temporaryDate}
               setSelectedDateTime={setTemporaryDate}
@@ -139,9 +133,9 @@ const CreateNewEventModal = ({ onClose, onNextStep }: CreateNewEventModalProps) 
         <div className="mt-12pxr relative flex w-full flex-col items-start justify-center">
           <ToggleButton
             className="absolute top-0 right-0"
-            state={conditions.keyword.isActive}
+            state={keyword.isActive}
             onToggle={toggleKeyword}
-            initialState={conditions.keyword.isActive}
+            initialState={keyword.isActive}
           />
           <CommonText type={CommonTextType.T6_SB_BLACK} text="특정 키워드 포함 여부" />
           <CommonText
@@ -149,7 +143,7 @@ const CreateNewEventModal = ({ onClose, onNextStep }: CreateNewEventModalProps) 
             text="특정 단어나 해시태그가 포함된 댓글만 선별해 드려요."
             className="mt-3pxr"
           />
-          {conditions.keyword.isActive && (
+          {keyword.isActive && (
             // <InputFieldModal
             //   placeholder="키워드를 입력해 주세요."
             //   value={keyword}
@@ -157,6 +151,7 @@ const CreateNewEventModal = ({ onClose, onNextStep }: CreateNewEventModalProps) 
             //   className="mt-8pxr"
             // />
             <InputChips
+              inputChips={keyword.keyword}
               placeholder="키워드를 입력해 주세요."
               onChipsChange={changeKeyword}
               className="mt-8pxr"
@@ -167,9 +162,9 @@ const CreateNewEventModal = ({ onClose, onNextStep }: CreateNewEventModalProps) 
         <div className="mt-12pxr relative flex w-full flex-col items-start justify-center">
           <ToggleButton
             className="absolute top-0 right-0"
-            state={conditions.friendTag.isActive}
+            state={friendTag.isActive}
             onToggle={toggleFriendTag}
-            initialState={conditions.friendTag.isActive}
+            initialState={friendTag.isActive}
           />
           <CommonText type={CommonTextType.T6_SB_BLACK} text="친구 태그 여부" />
           <CommonText
@@ -177,10 +172,10 @@ const CreateNewEventModal = ({ onClose, onNextStep }: CreateNewEventModalProps) 
             text="입력한 수 이상의 친구를 태그했는지 확인해 드려요."
             className="mt-3pxr"
           />
-          {conditions.friendTag.isActive && (
+          {friendTag.isActive && (
             <InputFieldModal
               placeholder="수를 입력해 주세요."
-              value={conditions.friendTag.requiredFriendTag?.toString() ?? ''}
+              value={friendTag.requiredFriendTag?.toString() ?? ''}
               onChange={handleRequiredFriendTagChange}
               className="mt-8pxr"
             />
@@ -189,7 +184,7 @@ const CreateNewEventModal = ({ onClose, onNextStep }: CreateNewEventModalProps) 
       </div>
 
       <div className="mt-16pxr flex w-full items-center justify-end">
-        <NextStepButton onClick={onNextStep} disabled={true} />
+        <NextStepButton onClick={onNextStep} disabled={false} />
       </div>
     </div>
   );
---
+++ src/common/hooks/stores/useSnsEventAssistantStore.ts
@@ -0,0 +1,19 @@
+import { useShallow } from 'zustand/shallow';
+
+import SnsEventAssistantStoreState from '@common/stores/sns-event-assistant-store';
+
+export const useSnsEventAssistantInfo = () =>
+  SnsEventAssistantStoreState(
+    useShallow((state) => ({
+      newTitle: state.newTitle,
+      newSnsEventLink: state.newSnsEventLink,
+      isLiked: state.conditions.isLiked,
+      isFollowed: state.conditions.isFollowed,
+      period: state.conditions.period,
+      keyword: state.conditions.keyword,
+      friendTag: state.conditions.friendTag,
+    })),
+  );
+
+export const useSnsEventAssistantActions = () =>
+  SnsEventAssistantStoreState((state) => state.actions);

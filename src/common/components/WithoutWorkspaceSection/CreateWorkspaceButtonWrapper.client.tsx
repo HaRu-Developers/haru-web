f75361a0f63620a581fedd41419e21c5abcfebaa
strfunctionk <comzun01@gmail.com>
2025-08-15 15:52:35 +0900

    ♻️ Refactor: landing 이동 및 workspace 버튼수정

--- src/common/components/WithoutWorkspaceSection/CreateWorkspaceButtonWrapper.client.tsx
+++ src/common/components/WithoutWorkspaceSection/CreateWorkspaceButtonWrapper.client.tsx
@@ -1,9 +1,15 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
+
+import { ROUTES } from '@common/constants/routes.constants';
+
 import CreateWorkspaceButton from '@common/components/buttons/38px/CreateWorkspaceButton/CreateWorkSpaceButton.client';
 
 const CreateWorkspaceButtonWrapper = () => {
-  return <CreateWorkspaceButton onClick={() => console.log()} />;
+  const Rotuer = useRouter();
+
+  return <CreateWorkspaceButton onClick={() => Rotuer.push(ROUTES.ONBOARDING)} />;
 };
 
 export default CreateWorkspaceButtonWrapper;
